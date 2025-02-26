9b1bfb1c7d87051753765f12958da9de3f9c0736
Jamala352 <ashlej2513@clarkstate.edu>
2025-02-26 18:11:07 -0500

    Update FixVariables.py

--- FixVariables.py
+++ FixVariables.py
@@ -12,3 +12,4 @@ theft = 2
 remaining = cakes - THEFT
 
 print(f"{thehello}, {theWorld}. There were {10cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
+"Hello", "world". There were 10 cakes, but Lex Luthor stole 2. Now there's 8. And that's sad.
