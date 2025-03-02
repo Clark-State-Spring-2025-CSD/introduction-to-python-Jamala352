09799ff621b58d526a229c16b6e358d41437cec6
Jamala352 <ashlej2513@clarkstate.edu>
2025-03-02 18:57:51 -0500

    Update FixVariables.py

--- FixVariables.py
+++ FixVariables.py
@@ -1,15 +1,14 @@
 ### Fix variables practice
-### Fix this file so that it runs. Do not add any aditional lines of code.
+### Fix this file so that it runs. Do not add any additional lines of code.
 ### Only change the variable names so the code runs.
-### The output should look like the follow line without the ###
+### The output should look like the following line without the ###
 ### Hello, world. There were 10 cakes, but Lex Luthor stole 2. Now there's 8. And that's sad.
 
-the Hello = "Hello"
-the.World = "world"
+theHello = "Hello"
+theWorld = "world"
 cakes = 10
 theft = 2
 
-remaining = cakes - THEFT
+remaining = cakes - theft
 
-print(f"{thehello}, {theWorld}. There were {10cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
-"Hello", "world". There were 10 cakes, but Lex Luthor stole 2. Now there's 8. And that's sad.
+print "Hello, world. There were 10 cakes, but Lex Luthor stole 2. Now there's 8. And that's sad".
